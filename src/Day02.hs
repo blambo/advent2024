63af944bb95aaa3fd5ccfd63defda86ce07ed941
Blambo <blambo@gmail.com>
2024-12-10 00:11:38 +1100

    Day 02 Part 2

--- src/Day02.hs
+++ src/Day02.hs
@@ -2,16 +2,21 @@ module Day02
   ( solve
   ) where
 
+
 solve :: IO ()
 solve = do
   contents <- readFile "./src/Day02Data.txt"
-  print $ safeCount $ map reportIsSafe $ getReports contents
+  print $ safeCount $ map checkReport $ getReports contents
 
 safeCount :: [Bool] -> Int
 safeCount (x:xs)
   | x==True = 1 + safeCount xs
   | otherwise = 0 + safeCount xs
 safeCount [] = 0
+
+
+checkReport :: [Int] -> Bool
+checkReport report = (reportIsSafe report) || (runWithProblemDampener report)
 
 
 -- A report is safe if it has good gaps between levels and levels are all increasing or decreasing
@@ -42,3 +47,20 @@ gapsHaveSameTrend [] = True
 
 getReports :: String -> [[Int]]
 getReports = map (map read . words) . lines
+
+
+runWithProblemDampener :: [Int] -> Bool
+runWithProblemDampener report = checkReportWithoutElem report $ length report
+
+checkReportWithoutElem :: [Int] -> Int -> Bool
+checkReportWithoutElem report idx
+  -- If we haven't found an element to remove that makes the report pass, then report is unsafe
+  | idx < 0 = False
+  | foo report idx = True
+  -- Keep looking for a safe report after removing an elem
+  | otherwise = checkReportWithoutElem report (idx-1)
+
+foo :: [Int] -> Int -> Bool
+foo report x =
+  let (h, t) = splitAt x report in
+    reportIsSafe (h ++ (drop 1 t))
